a90909375a59d81707e3d892802fa0b554cdc433
kenwoo9y <ei.1px.9.01@gmail.com>
2021-08-06 22:08:22 +0900

    First commit

---
+++ src/main/java/com/example/inquiryformsample/InquiryFormSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.inquiryformsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryFormSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryFormSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inquiryformsample/InquiryFormSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inquiryformsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquiryFormSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
