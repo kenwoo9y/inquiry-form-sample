b1e7213e6366f86bf7376e129516ab78a846d478
kenwoo9y <ei.1px.9.01@gmail.com>
2021-08-10 23:44:30 +0900

    database setting

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('AAA', 'aaa@example.com', 'Hello', '2021-08-10 08:34:19');
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('BBB', 'bbb@example.com', 'GoodMorning', '2021-09-12 12:10:52');
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('CCC', 'ccc@example.com', 'GoodNight', '2021-12-31 15:10:52');
